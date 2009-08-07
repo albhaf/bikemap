0affa932de72e32b0bcdaf8d30ae1a1c74ec100b
Albert Hafvenström <albhaf@gmail.com>
2009-08-07 18:53:12 +0200

    clean up and minor changes in javascript code, added sample servlate and initiated eclipse project

--- citybikes/js/bikemap.js
+++ citybikes/js/bikemap.js
@@ -1,35 +1,79 @@
+
+var stationsArray = new Array();
+	
 var map = null;
 var geocoder = null;
 
 var maxDistance = 1000;
 var maxStations = 10;
 
-var activeMarker = new GMarker(new GLatLng(59.32452, 18.071136));
+var address = null;
+var point = null;
+
+// arrays to hold copies of the markers used by the side_bar
+// because the function closure trick doesnt work there
+var gmarkers = [];
+
+// this variable will collect the html which will eventually be placed in the side_bar
+var side_bar_html = '';
 
 if(GBrowserIsCompatible()) {
 	
-	
-	// arrays to hold copies of the markers used by the side_bar
-	// because the function closure trick doesnt work there
-	var gmarkers = [];
 
-	// this variable will collect the html which will eventually be placed in the side_bar
-	var side_bar_html = '';
+	var activeMarker = new GMarker(new GLatLng(59.32452, 18.071136));
 	
 	var bounds = new GLatLngBounds();
+	
+	function citybikeStation(point, label, html) {
+		this.point = point;
+		this.label = label;
+		this.html = html;
+	}
+	function loadStations() {
+		
+			GDownloadUrl('citybikes.kml', function(doc) {
+				var xmlDoc = GXml.parse(doc);
+				var markers = xmlDoc.documentElement.getElementsByTagName('Placemark');
+				
+				
+				for(var j = 0; j < markers.length; j++) {
+					// obtain the attribues of each marker
+					var coords = markers[j].getElementsByTagName('coordinates')[0].firstChild.nodeValue.split(',');
+					var lng = parseFloat(coords[0]);
+					var lat = parseFloat(coords[1]);
+					var point = new GLatLng(lat,lng);
+					bounds.extend(point);
 
-	function mapInit() {
-		mapDraw(true, null, null);
-		var mapBounds = map.getBounds();
-		geocoder = new GClientGeocoder();
-		geocoder.setViewport(mapBounds);
+					var label = markers[j].getElementsByTagName('name')[0].firstChild.nodeValue;
+					var htmlNode = markers[j].getElementsByTagName('description')
+					var html = label;
+					if(htmlNode.length > 0) {
+						html = htmlNode[0].firstChild.nodeValue;
+					}
+					
+					stationsArray[j] = new citybikeStation(point,label,html);
+					
+				}
+			});
+			
+			//Script is executed too fast for stationsArray to be populated
+			setTimeout("mapDraw(true,true,null,null)", 200);
+		
 	}
 	
-	function mapDraw(allMarkers, point, address) {
+	function mapDraw(allMarkers, stationsLoad, point, address) {		
+		//~ if(stationsLoad) {
+			//~ loadStations();
+		//~ }
+		//~ var t=setTimeout("", 500);
+		
 		map = new GMap2(document.getElementById('map'));
 		map.addControl(new GLargeMapControl());
 		map.addControl(new GMapTypeControl());
-		
+		map.setCenter(new GLatLng(59.32452, 18.071136), 12);
+		var mapBounds = map.getBounds();
+		geocoder = new GClientGeocoder();
+		geocoder.setViewport(mapBounds);
 		
 		if(point) {
 			map.setCenter(point, 14);
@@ -45,104 +89,30 @@ if(GBrowserIsCompatible()) {
 			map.addOverlay(marker);
 			
 			//~ marker.openInfoWindowHtml(address);
-		} else {
-			map.setCenter(new GLatLng(59.32452, 18.071136), 12);
 		}
+		
 		
 		if(allMarkers) {
 			drawAllMarkers();
 		} else {
 			drawClosestMarkers(point);
-			//~ drawMarkersWithinKM(point);
 		}
 	}
 
 
 	function drawAllMarkers() {
-		
-		
-		// Read the data from citybikes.kml
-		GDownloadUrl('citybikes.kml', function(doc) {
-			var xmlDoc = GXml.parse(doc);
-			var markers = xmlDoc.documentElement.getElementsByTagName('Placemark');
-
 			//Reset side_bar_html
 			side_bar_html = '';
 			
 			side_bar_html += '<br><table width="100%">';
 			
-			for (var i = 0; i < markers.length; i++) {
-			  // obtain the attribues of each marker
-			  var coords = markers[i].getElementsByTagName('coordinates')[0].firstChild.nodeValue.split(',');
-			  var lng = parseFloat(coords[0]);
-			  var lat = parseFloat(coords[1]);
-			  var point = new GLatLng(lat,lng);
-			  bounds.extend(point);
-
-			  var label = markers[i].getElementsByTagName('name')[0].firstChild.nodeValue;
-			  var htmlNode = markers[i].getElementsByTagName('description')
-			  var html = label;
-			  if(htmlNode.length > 0) {
-				html = htmlNode[0].firstChild.nodeValue;
-			  }
-
+			
+			
+			for (var i = 0; i < stationsArray.length; i++) {
 			  // create the marker
-			  var marker = createMarker(point,label,html);
+			  var marker = createMarker(stationsArray[i].point,stationsArray[i].label,stationsArray[i].html);
 			  map.addOverlay(marker);
 			}
-
-			
-			showAll();
-			
-			// showAll and closing tag for side_bar_html
-			side_bar_html += '<a href="javascript:showAll()">Show all<\/a><br>\n';
-			side_bar_html += '</table>';
-
-			// put the assembled side_bar_html contents into the results div
-			document.getElementById('results').innerHTML = side_bar_html;
-		  });
-	}
-
-	function drawMarkersWithinKM(addressPoint) {
-		// Read the data from citybikes.kml
-		
-		GDownloadUrl('citybikes.kml', function(doc) {
-			var xmlDoc = GXml.parse(doc);
-			var markers = xmlDoc.documentElement.getElementsByTagName('Placemark');
-
-			//Reset side_bar_html
-			side_bar_html = '';
-			
-			side_bar_html += '<br><table width="100%">';
-						
-			
-			for (var i = 0; i < markers.length; i++) {				 
-				// obtain the attribues of each marker
-				var coords = markers[i].getElementsByTagName('coordinates')[0].firstChild.nodeValue.split(',');
-				var lng = parseFloat(coords[0]);
-				var lat = parseFloat(coords[1]);
-				var point = new GLatLng(lat,lng);
-				bounds.extend(point);
-
-				var label = markers[i].getElementsByTagName('name')[0].firstChild.nodeValue;
-				var htmlNode = markers[i].getElementsByTagName('description')
-				var html = label;
-				if(htmlNode.length > 0) {
-				html = htmlNode[0].firstChild.nodeValue;
-				}
-				
-				
-				var stationDistance = point.distanceFrom(addressPoint);
-				
-				if(stationDistance <= maxDistance) {
-					// create the marker
-					var marker = createMarker(point,label,html, stationDistance);
-					map.addOverlay(marker);
-				}
-			}
-
-			
-			//~ showAll();
 			
 			// showAll and closing tag for side_bar_html
 			//~ side_bar_html += '<a href="javascript:showAll()">Show all<\/a><br>\n';
@@ -150,93 +120,39 @@ if(GBrowserIsCompatible()) {
 
 			// put the assembled side_bar_html contents into the results div
 			document.getElementById('results').innerHTML = side_bar_html;
-		  });
+		  //~ });
+		  
 	}
+
 	
-	function citybikeStation(point, label, html, distance) {
-		this.point = point;
-		this.label = label;
-		this.html = html;
-		this.distance = distance;
-	}
-	
-	function sortMarkersByDistance(a, b) {
-		return(a.distance - b.distance);
-	}
 	
 	function drawClosestMarkers(addressPoint) {
-		// Read the data from citybikes.kml
-		
-		GDownloadUrl('citybikes.kml', function(doc) {
-			var xmlDoc = GXml.parse(doc);
-			var markers = xmlDoc.documentElement.getElementsByTagName('Placemark');
+			var sortedMarkersArray = stationsArray.sort(function(a, b) {  return (a.point.distanceFrom(addressPoint) - b.point.distanceFrom(addressPoint));   });
 			
-			var markersArray = new Array();
-			var distanceArray = new Array();
-			
-			// obtain the attribues of each marker
-			
-			for(var f = 0; f < markers.length; f++) {
-				var coords = markers[f].getElementsByTagName('coordinates')[0].firstChild.nodeValue.split(',');
-				var lng = parseFloat(coords[0]);
-				var lat = parseFloat(coords[1]);
-				var point = new GLatLng(lat,lng);
-				bounds.extend(point);
-
-				var label = markers[f].getElementsByTagName('name')[0].firstChild.nodeValue;
-				var htmlNode = markers[f].getElementsByTagName('description')
-				var html = label;
-				if(htmlNode.length > 0) {
-				html = htmlNode[0].firstChild.nodeValue;
-				}
-				
-				var stationDistance = point.distanceFrom(addressPoint);
-
-				markersArray[f] = new citybikeStation(point, label, html, stationDistance);
-			}
-			
-			
-			var sortedMarkersArray = markersArray.sort(sortMarkersByDistance);
-				
-		
-
 			//Reset side_bar_html
 			side_bar_html = '';
 			
 			side_bar_html += '<br><table width="100%">';
-			
-							
 			
 			for (var i = 0; i < maxStations; i++) {				 
 			// create the marker
-			var marker = createMarker(sortedMarkersArray[i].point,sortedMarkersArray[i].label,sortedMarkersArray[i].html, sortedMarkersArray[i].distance);
+			var marker = createMarker(sortedMarkersArray[i].point,sortedMarkersArray[i].label,sortedMarkersArray[i].html, sortedMarkersArray[i].point.distanceFrom(addressPoint));
 			map.addOverlay(marker);
 			}
 
-			
-			//~ showAll();
-			
-			// showAll and closing tag for side_bar_html
-			//~ side_bar_html += '<a href="javascript:showAll()">Show all<\/a><br>\n';
+			// closing tag for side_bar_html
 			side_bar_html += '</table>';
 
 			// put the assembled side_bar_html contents into the results div
 			document.getElementById('results').innerHTML = side_bar_html;
-		  });
+		  //~ });
 	}
 	
-	 function showAll() {
-		//Closes the previously set active marker.
-		activeMarker.closeInfoWindow();
-		
-		map.setZoom(map.getBoundsZoomLevel(bounds));
-		map.setCenter(bounds.getCenter());
-  }
 			
 
 	function clickMarker(i) {
 		GEvent.trigger(gmarkers[i], 'click');
-		map.setCenter(gmarkers[i].getLatLng(),14);
+		//~ map.setCenter(gmarkers[i].getLatLng(),14);
 	}
 
 	function createMarker(point, name, html, distance) {
@@ -257,9 +173,9 @@ if(GBrowserIsCompatible()) {
 		
 		if(distance) {
 			// add a station to the side_bar html
-			side_bar_html += '<tr><td><font size="-1"><a href="javascript:clickMarker(' + (gmarkers.length-1) + ')">' + name + '</a></font></td><font size="-1"><td style="white-space: nowrap" align="right">' + parseInt(distance) + ' <em>m</em></td></font></tr>';
+			side_bar_html += '<tr><td class="stationName"><a href="javascript:clickMarker(' + (gmarkers.length-1) + ')">' + name + '</a></td><td class="stationDistance" style="white-space: nowrap" align="right">' + parseInt(distance) + ' <em>m</em></td></tr>';
 		} else {
-			side_bar_html += '<tr><td><font size="-1"><a href="javascript:clickMarker(' + (gmarkers.length-1) + ')">' + name + '</a></font></td><font size="-1"><td style="white-space: nowrap" align="right"></td></font></tr>';
+			side_bar_html += '<tr><td class="stationName"><a href="javascript:clickMarker(' + (gmarkers.length-1) + ')">' + name + '</a></td><td class="stationDistance" style="white-space: nowrap" align="right"></td></tr>';
 		 }
 
 		return marker;
@@ -274,14 +190,15 @@ if(GBrowserIsCompatible()) {
 				if (!point) {
 				  alert(address + " not found");
 				} else {
-				  mapDraw(false, point, address);
+				  mapDraw(false, false, point, address);
 				}
 			  }
 			);
 		  }
 	  } else {
-		  mapDraw(true, null, null);
+		  mapDraw(true, false, null, null);
 	  }
+	  //~ alert("stationsArray.length = " + stationsArray.length);
 	}
 	
 	
